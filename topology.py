132d4701fb407bc815d4bed5e4dabddb9969274d
Ulises Nieva <unieva@fi.uba.ar>
2025-11-13 16:19:53 -0300

    Agrego topologia

---
+++ topology.py
@@ -0,0 +1,30 @@
+from mininet.topo import Topo
+
+class CustomTopo(Topo):
+    def __init__(self, num_switches):
+        # Initialize topology
+        Topo.__init__(self)
+
+        # Create switches
+        switches = []
+        for i in range(num_switches):
+            s = self.addSwitch(f'switch_{i+1}')
+            switches.append(s)
+                               
+        # Create hosts
+        h1 = self.addHost('host_1')
+        h2 = self.addHost('host_2')
+        h3 = self.addHost('host_3')
+        h4 = self.addHost('host_4')
+
+        # Add links between switches and hosts
+        self.addLink(switches[0], h1)
+        self.addLink(switches[0], h2)
+        self.addLink(switches[-1], h3)
+        self.addLink(switches[-1], h4)
+
+        # Add links between switches
+        for i in range(len(switches) - 1):
+            self.addLink(switches[i], switches[i + 1])
+
+topos = { 'customTopo': CustomTopo }
